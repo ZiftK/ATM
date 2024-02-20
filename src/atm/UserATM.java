2181c080c12eb4349f96f0cd760ab0dfe909c9b2
ZiftK <alvaro.cab.lag@gmail.com>
2024-02-20 07:52:02 -0800

    add: load method docs

--- src/atm/UserATM.java
+++ src/atm/UserATM.java
@@ -17,27 +17,43 @@ public class UserATM extends ATM{
 
     @Override
     public void load(){
-        super.load();
 
+        // We execute the load method of the superclass
+        super.load();
+        // ATM input key
         int input;
 
+        /*
+        While the user does not enter a valid key, we maintain the loop
+        */
         while (true){
 
+            // get key
             input = msg.getIntFromInput("Introduce tu clave");
 
+            /*
+            If the key is in the records, we stop the looping; if
+            it is not in the records, we continue with the loop
+            and throw an error message
+            */
             if (clientsTable.exists(input)) break;
+
             msg.error("La clave ingresada no existe en los registros");
 
         }
 
+        /*At this point, we already have an existing key, so we
+        retrieve the item in the table with that key and
+        load the customer data*/
         int finalInput = input;
         ArrayList<KeyFileItem> keyItem = clientsTable.find(item -> item.key == finalInput);
 
-        CsvTable<Client> ctbl = new CsvTable<>(keyItem.get(0).getFilePath(),new Client());
-        ctbl.loadRecord();
-        clientData = ctbl.items().get(0);
+        CsvTable<Client> ctl = new CsvTable<>(keyItem.get(0).getFilePath(),new Client());
+        ctl.loadRecord();
+        clientData = ctl.items().get(0);
     }
     public void save(){
+
 
     }
 
